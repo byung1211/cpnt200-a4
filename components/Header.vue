31fd8c25ac26c6d4abf691b276951b4977e8567b
Alex An <ahn1211@gmail.com>
2021-11-25 17:33:39 -0700

    Changed the bg color of navigation

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-indigo-100 py-3">
+  <header class="bg-green-200 py-3">
     <div class="container flex flex-wrap items-center justify-center py-3 mx-auto">
       <h1 class="text-3xl md:text-4xl font-medium mb-2text-3xl md:text-4xl font-medium mb-2">{{siteInfo.title}}</h1>
     </div>
